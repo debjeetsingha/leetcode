5e068ccf50222e1fa62c5ac437cb264ee1306277
debjeetsingha <debjeetsingha2003@gmail.com>
2026-04-15 02:03:33 +0530

    solved longest-common-prefix.py

---
+++ python/easy/longest-common-prefix.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def is_prefix(idx:int, char: str, strs: List[str]):
+    for x in strs:
+        if len(x)<=idx or x[idx]!=char:
+            return False
+    return True
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ''
+        word1=strs[0]
+        for idx, char in enumerate(word1):
+            if not(is_prefix(idx, char, strs)):
+                return prefix
+            prefix = prefix+char
+        return prefix
