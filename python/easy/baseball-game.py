c661bf6e02821f43f9039551c231ae8c3f683dc4
debjeetsingha <debjeetsingha2003@gmail.com>
2026-04-20 17:26:36 +0530

    Solved baseball game

---
+++ python/easy/baseball-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for x in (operations):
+            if x =="C":
+                record.pop()
+            elif x =="+":
+                record.append(record[-2] + record[-1])
+            elif x =="D":
+                record.append(2*int(record[-1]))
+            else:
+                record.append(int(x))
+        return sum(record)
