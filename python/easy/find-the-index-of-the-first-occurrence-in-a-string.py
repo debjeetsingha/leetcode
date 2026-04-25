29e2f9f23eca5d572fb8bb89d2f854278788bcf4
debjeetsingha <debjeetsingha2003@gmail.com>
2026-04-25 19:51:44 +0530

    modified leetcode 28 to use manual sliding window

--- python/easy/find-the-index-of-the-first-occurrence-in-a-string.py
+++ python/easy/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,3 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        # return haystack.find(needle)
+        l = 0
+        r = len(needle)
+        while r <= len(haystack):
+            if haystack[l:r]==needle:
+                return l
+            l+=1
+            r+=1
+        return -1
