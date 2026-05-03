5fc114dc2973a3a030495d0fcf2577751831937e
debjeetsingha <debjeetsingha2003@gmail.com>
2026-05-03 21:24:21 +0530

    added recursive solution to 206

--- python/easy/reverse-linked-list.py
+++ python/easy/reverse-linked-list.py
@@ -1,8 +1,14 @@
+"""
+Link: https://leetcode.com/problems/reverse-linked-list/
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# ITERATIVE 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prevNode = None
@@ -14,3 +20,16 @@ class Solution:
             curNode = nextNode
             
         return prevNode
+
+
+# RECURSIVE
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        nextHead = self.reverseList(head.next)
+        head.next.next=head
+        head.next=None
+
+        return nextHead
