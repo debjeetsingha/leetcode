89327d4381a5cdb8f9e5f33b9b13bebdae7f9adc
debjeetsingha <debjeetsingha2003@gmail.com>
2026-05-08 20:15:18 +0530

    solved 303 but brute force

---
+++ python/easy/range-sum-query-immutable.py
@@ -0,0 +1,18 @@
+"""
+Link: https://leetcode.com/problems/range-sum-query-immutable/
+"""
+
+# brute force approach
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        return sum(self.nums[left:right+1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
