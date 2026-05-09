2da437756f9660e3c3fe77201dc19b3e7f19f656
Debjeet Singha <51025635+debjeetsingha@users.noreply.github.com>
2026-05-09 17:14:44 +0530

    Add prefix sum implementation to leetcode 303
    Implemented prefix sum approach

--- python/easy/range-sum-query-immutable.py
+++ python/easy/range-sum-query-immutable.py
@@ -13,6 +13,21 @@ class NumArray:
         return sum(self.nums[left:right+1])
 
 
+# using prefix sums
+
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.prefix = []
+        cur_sum = 0
+        for x in nums:
+            cur_sum+=x
+            self.prefix.append(cur_sum)
+            
+    def sumRange(self, left: int, right: int) -> int:
+        if left==0:
+            return self.prefix[right]
+        return self.prefix[right] - self.prefix[left-1]
+
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
