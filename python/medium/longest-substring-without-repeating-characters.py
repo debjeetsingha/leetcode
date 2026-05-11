8ffd4a94cc8365b1b8b44724b41bb98f41d0ded9
debjeetsingha <debjeetsingha2003@gmail.com>
2026-05-11 10:36:39 +0530

    modified leetcode 3

--- python/medium/longest-substring-without-repeating-characters.py
+++ python/medium/longest-substring-without-repeating-characters.py
@@ -1,7 +1,6 @@
 """
 Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-update approach later
 """
 
 class Solution:
@@ -9,28 +8,17 @@ class Solution:
         if len(s)<=1:
             return len(s)
         max_len = 0
-        str_det = {} # {str: [count, last_seen_idx]}
+        str_det = {} 
         l=0
-        cur_count = 0
         for r in range(len(s)):
             if s[r] not in str_det:
-                str_det[s[r]] = [0,-1]
-                
-            str_det[s[r]][0] += 1
-            str_det[s[r]][1] = r
-            cur_count += 1
+                str_det[s[r]] = 0
 
+            str_det[s[r]] += 1
 
-            while str_det[s[r]][0] > 1:
-                str_det[s[l]][0] -= 1
+            while str_det[s[r]] > 1:
+                str_det[s[l]] -= 1
                 l = l+1
-                cur_count-=1
 
-
-            max_len = max(max_len, cur_count)
+            max_len = max(max_len, r-l+1)
         return max_len
-
-
-
-
-
