f97b1290845e94a5ee35a0fc6fbc9a7f7fac5ad5
debjeetsingha <debjeetsingha2003@gmail.com>
2026-05-19 21:31:32 +0530

    solved 24

---
+++ python/medium/swap-nodes-in-pairs.py
@@ -0,0 +1,24 @@
+"""
+LEETCODE 24: https://leetcode.com/problems/swap-nodes-in-pairs/
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        cur = dummy
+        while cur.next and cur.next.next:
+            first = cur.next
+            second = cur.next.next
+
+            first.next = second.next
+            second.next = first
+            cur.next = second
+
+            cur = first
+
+        return dummy.next
